4e0aa0747b86e2c1d34dc5b2d8e6b179ce1f5c6e
facutapiaa <facu_tapiaa@hotmail.com>
2024-04-10 10:56:09 -0300

    creamos los modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './modulos/inicio/inicio.component';
+import { GaleriaComponent } from './modulos/galeria/galeria.component';
+import { ServiciosComponent } from './modulos/servicios/servicios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"inicio",component:InicioComponent},
+  {path:"galeria",component:GaleriaComponent},
+  {path:"servicios",component:ServiciosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
