6e19a57ca125ff1523f8faecf9a5217cf3b2e8a6
facutapiaa <facu_tapiaa@hotmail.com>
2024-06-05 10:21:20 -0300

    logica del inicio sesion

--- src/app/modules/autentificacion/pages/iniciosesion/iniciosesion.component.ts
+++ src/app/modules/autentificacion/pages/iniciosesion/iniciosesion.component.ts
@@ -1,10 +1,67 @@
+import { FocusTrap } from '@angular/cdk/a11y';
 import { Component } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
 
 @Component({
   selector: 'app-iniciosesion',
   templateUrl: './iniciosesion.component.html',
   styleUrls: ['./iniciosesion.component.css']
 })
+
 export class IniciosesionComponent {
-  hide=true
+  hide = true;
+
+  //importar la interfaz de usuario inizialisada
+  inicio: Usuario = {
+    uid: '',
+    nombre: '',
+    apellido: '',
+    email: '',
+    rol: '',
+    password: '',
+  }
+
+  //creamos una coleccion de usuarios tipo 'usuario' para arrays
+  coleccionInicio: Usuario[] = []
+
+  //creamos la funcion crear que se activa con el boton
+  iniciar() {
+    const inicial = {
+      email: this.inicio.email,
+      password: this.inicio.password,
+      nombre: this.inicio.nombre,
+      apellido: this.inicio.apellido,
+      rol: this.inicio.rol,
+      uid: this.inicio.uid,
+    }
+
+    //metemos la constante dentro de la coleccion
+    this.coleccionInicio.push(inicial);
+
+    //traemos los datos del registro
+    const contraseña =localStorage.getItem('contraseña')
+    const correo =localStorage.getItem('email')
+
+    //condicionales para  iniciar session
+    if(contraseña===this.inicio.password && this.inicio.email){
+      alert("se inicio sesion")
+    }else{
+      alert("tamos mal papito")
+    } 
+  }
 }
+
+
+ /*const datos = localStorage.getItem('user')
+     if (datos) {
+      const usuario = JSON.parse(datos)
+      console.log(usuario)
+      for (let i = 0; i <= usuario.length; i++) {
+        if (usuario.email === this.inicio.email) {
+          if (usuario.password === this.inicio.password) {
+            alert("se inicio correctamente sesion")
+          }
+        }
+      }
+    }*/
+
--- src/app/modules/autentificacion/pages/registro/registro.component.ts
+++ src/app/modules/autentificacion/pages/registro/registro.component.ts
@@ -19,22 +19,49 @@ export class RegistroComponent {
     password: '',
   }
 
-  //creamos una coleccion de usuarios tipoo 'usuario' para arrays
+  //creamos una coleccion de usuarios tipo 'usuario' para arrays
   coleccionUsuarios: Usuario[] = []
 
   //funcion para el registro de nuevos usuarios
   registrar() {
     //constante que guarda la informacion que ingresa el usuario
     const credenciales = {
+      email: this.usuarios.email,
+      password: this.usuarios.password,
       uid: this.usuarios.uid,
       nombre: this.usuarios.nombre,
       apellido: this.usuarios.apellido,
-      email: this.usuarios.email,
       rol: this.usuarios.rol,
-      password: this.usuarios.password
+
     }
-    //mostramos las credenciales por consola
-    console.log(credenciales)
+
+    //enviamos nueva informacion como un nuevo objeto a la coleccion de usuarios
+    this.coleccionUsuarios.push(credenciales);
+
+    alert("te registraste con exito")
+
+    //lamamos a la funcion para ejecutarla
+    this.limpiarInputs();
+
+    //usamos local storage para guardar los datos
+    localStorage.setItem('email', this.usuarios.email)
+    localStorage.setItem('contraseña', this.usuarios.password)
+    /*
+    localStorage.setItem('user', JSON.stringify(this.coleccionUsuarios))
+    */
+  }
+
+  //funcion para limpiar inputs
+  limpiarInputs() {
+
+    //en la constante inputs llamamos los atributos y los inicializamos vacios
+    const inputs = {
+      email: this.usuarios.email = '',
+      password: this.usuarios.password = '',
+      uid: this.usuarios.uid = '',
+      nombre: this.usuarios.nombre = '',
+      apellido: this.usuarios.apellido = '',
+      rol: this.usuarios.rol = '',
+    }
   }
 }
-
