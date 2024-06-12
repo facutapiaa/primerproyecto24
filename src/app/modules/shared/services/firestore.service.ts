f1ac31f88ef2bca153443bb8905b3cf99a2ee51a
facutapiaa <facu_tapiaa@hotmail.com>
2024-06-12 10:01:04 -0300

    creamos un nuevo servicio llamado 'firestore' para acceder al cloud firestore de firebase y definimos una coleccion para los usuarios

---
+++ src/app/modules/shared/services/firestore.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { AngularFireStorage, AngularFireStorageModule } from '@angular/fire/compat/storage';
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreService {
+
+  constructor() { }
+}
